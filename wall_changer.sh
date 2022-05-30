1488b8410a7824a9237d546b6862400fc73f86a9
Remy <remy.noulin@epitech.eu>
2022-05-30 19:30:58 +0200

    [MOD] log system

--- wall_changer.sh
+++ wall_changer.sh
@@ -2,4 +2,4 @@
 
 cd /opt/wallpaper_changer/;
 git pull;
-gsettings get org.gnome.desktop.background picture-uri 'file:///opt/wallpaper_changer/$(whoami)/wallpaper.png'
+gsettings get org.gnome.desktop.background picture-uri 'file:///opt/wallpaper_changer/$(whoami)/wallpaper.png' >> log
