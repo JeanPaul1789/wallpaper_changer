15ff0d6b46c44bf4b7b0c185557f32fd4bd8cde9
Remy <remy.noulin@epitech.eu>
2022-05-31 14:19:59 +0200

    [MOD] add gitignore and use feh to set background

--- wall_changer.sh
+++ wall_changer.sh
@@ -2,4 +2,9 @@
 
 cd /opt/wallpaper_changer/;
 git pull;
-gsettings set org.gnome.desktop.background picture-uri file:///opt/wallpaper_changer/$(whoami)/wallpaper.png >> log
+if [[ -d $(whoami) ]];
+then
+    feh --bg-scale $(whoami)/wallpaper.png
+else
+    echo "Directory of user $(whoami) not found."
+fi
